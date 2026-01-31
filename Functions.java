5310ece821142aa08c43eec16b0c83c5718678e9
Jairaj Choudhary <choudharyjairaj569@gmail.com>
2026-01-31 23:19:26 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,18 @@
+public class Functions {
+
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+
+    // public static int printHelloWorld() {
+    //     System.out.println("Hello World");
+    //     System.out.println("Hello World");
+    //     System.out.println("Hello World");
+    //     return 3;
+    // }
+    public static void main(String[] args) {
+        printHelloWorld();     //Function call
+    }
+}
