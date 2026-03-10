5ee3676077a235e65d11819b50c3408893a5f61d
Jairaj Choudhary <choudharyjairaj569@gmail.com>
2026-03-10 22:48:17 +0530

    Add files via upload

--- Functions.java
+++ Functions.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Functions {
 
     public static void printHelloWorld() {
@@ -12,7 +13,44 @@ public class Functions {
     //     System.out.println("Hello World");
     //     return 3;
     // }
-    public static void main(String[] args) {
-        printHelloWorld();     //Function call
+    // public static void main(String[] args) {
+    //     printHelloWorld();     Function call
+    // }
+
+    // public static void calculatesum() {
+    //     Scanner Sc = new Scanner (System.in);
+    //     int a = Sc.nextInt();
+    //     int b = Sc.nextInt();
+    //     int sum = a + b;
+    //     System.out.println("Sum is = " + sum);
+    // }
+
+    // public static void main(String args[]) {
+    //     calculatesum();
+    // }
+
+    // public static void calculatesum(int a, int b) {
+    //     int sum = a + b;
+    //     System.out.println("Sum is = " + sum);
+    // }
+
+    // public static void main(String args[]) {
+    //     Scanner Sc = new Scanner (System.in);
+    //     int a = Sc.nextInt();
+    //     int b = Sc.nextInt();
+    //     calculatesum(a, b);
+    // }
+
+    public static int calculateSum(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        Scanner Sc = new Scanner (System.in);
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+        int sum = calculateSum(a, b);
+        System.out.println("Sum is = " + sum);
     }
 }
